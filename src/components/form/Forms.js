f0ab517a7b8eff6e80e721e2e127309a45de2900
chajoonho <144306261+yeon7513@users.noreply.github.com>
2024-08-20 19:37:07 +0900

    20240820

--- src/components/form/Forms.js
+++ src/components/form/Forms.js
@@ -1,6 +1,6 @@
-import React from "react";
-import styles from "./Form.module.scss";
-import { useForm } from "react-hook-form";
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import styles from './Form.module.scss';
 
 function Forms({ title, getDataForm, firebaseError }) {
   const {
@@ -9,7 +9,7 @@ function Forms({ title, getDataForm, firebaseError }) {
     formState: { errors },
     reset,
   } = useForm({
-    mode: "onChange",
+    mode: 'onChange',
   });
   const onSubmit = ({ email, password, userInfo }) => {
     getDataForm(email, password);
@@ -17,20 +17,20 @@ function Forms({ title, getDataForm, firebaseError }) {
   };
 
   const userEmail = {
-    required: "필수 필드입니다.",
+    required: '필수 필드입니다.',
   };
 
   const userPassword = {
-    required: "필수 필드입니다.",
+    required: '필수 필드입니다.',
     minLength: {
       value: 8,
-      message: "최소 8자 이상.",
+      message: '최소 8자 이상.',
     },
     pattern: {
       // value:
       //   /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
       message:
-        "대문자, 소문자, 숫자, 특수 문자를 포함한 최소 8자 이상이어야 합니다.",
+        '대문자, 소문자, 숫자, 특수 문자를 포함한 최소 8자 이상이어야 합니다.',
     },
   };
 
@@ -40,7 +40,7 @@ function Forms({ title, getDataForm, firebaseError }) {
         <input
           type="email"
           placeholder="이메일 입력"
-          {...register("email", userEmail)}
+          {...register('email', userEmail)}
         />
         {errors?.email && (
           <div>
@@ -52,7 +52,7 @@ function Forms({ title, getDataForm, firebaseError }) {
         <input
           type="password"
           placeholder="비밀번호 입력(문자, 숫자, 특수문자 포함 8~20자)"
-          {...register("password", userPassword)}
+          {...register('password', userPassword)}
         />
         {errors?.password && (
           <div>
@@ -64,7 +64,7 @@ function Forms({ title, getDataForm, firebaseError }) {
         <input
           type="password"
           placeholder="비밀번호 재입력"
-          {...register("password", userPassword)}
+          {...register('password', userPassword)}
         />
         {errors?.password && (
           <div>
