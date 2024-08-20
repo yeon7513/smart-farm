7ea04d6558624819fd5430c361cea7673a2c994a
je <wjddmsy12345@gmail.com>
2024-08-20 16:03:30 +0900

    .

--- src/pages/about/Service.js
+++ src/pages/about/Service.js
@@ -1,7 +1,16 @@
 import React from "react";
-
+import styles from "./Service.module.scss";
+import smtImg from "../../assets/abou/스마트팜.png";
 function Service(props) {
-  return <div></div>;
+  return (
+    <div>
+      <div className={styles.farm}>
+        <div>
+          <img src={styles.smtImg} />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Service;
