c771611a7fc4ce6db590c0b5ece988a1c998e1c7
yeeun <48862667+goyangyee22@users.noreply.github.com>
2024-08-20 14:20:20 +0900

    update

--- src/pages/customer-service-center/CustomerServiceCenter.js
+++ src/pages/customer-service-center/CustomerServiceCenter.js
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import styles from "./CustomerServiceCenter.module.scss";
 
 function CustomerServiceCenter() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const toggleVisibility = () => {
+    setIsVisible((prevState) => !prevState);
+  };
+
   return (
     <>
-      <div className="faq">
-        <div className="title">
+      <div className={styles.faq}>
+        <div className={styles.title}>
           <h3>제?목</h3>
-          <button>눌러보셈</button>
+          {!isVisible ? (
+            <button onClick={toggleVisibility}>화살표 아래 버튼</button>
+          ) : (
+            <button onClick={toggleVisibility}>화살표 위 버튼</button>
+          )}
         </div>
-        <div className="description">내?용</div>
+        {isVisible && (
+          <div className={styles.description}>
+            <h4>내?용</h4>
+          </div>
+        )}
       </div>
     </>
   );
