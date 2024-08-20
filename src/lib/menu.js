bb37c91cd2aec97e8e9eb3235d68cef15a514bd5
chajoonho <144306261+yeon7513@users.noreply.github.com>
2024-08-20 13:40:15 +0900

    20240820

--- src/lib/menu.js
+++ src/lib/menu.js
@@ -1,9 +1,39 @@
 export const paths = {
   gnb: [
     { path: '/', name: '홈' },
-    { path: 'about', name: '소개', depth: {} },
-    { path: 'simulation', name: '시뮬레이션' },
-    { path: '/', name: '고객센터' },
+    {
+      path: 'about',
+      name: '소개',
+      depth: [
+        {
+          path: '/',
+          name: '회사소개',
+        },
+        { path: '/', name: '서비스소개' },
+      ],
+    },
+    {
+      path: 'simulation',
+      name: '시뮬레이션',
+      depth: [
+        {
+          path: '/',
+          name: '과채',
+        },
+        { path: '/', name: '과수' },
+      ],
+    },
+    {
+      path: '/',
+      name: '고객센터',
+      depth: [
+        {
+          path: '/',
+          name: '견적 요청',
+        },
+        { path: '/', name: 'FAQ' },
+      ],
+    },
   ],
   spot: [
     { path: '/login', name: '로그인' },
