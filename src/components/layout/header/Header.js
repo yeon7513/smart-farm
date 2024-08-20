5b24a6658581c59abace23286b05014cf055fa9a
chajoonho <wnsgh882002@gmail.com>
2024-08-20 11:32:04 +0900

    20240820

--- src/components/layout/header/Header.js
+++ src/components/layout/header/Header.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Container from '../container/Container';
+import styles from './Header.module.scss';
 import Nav from './nav/Nav';
 
-function Header(props) {
+function Header() {
   return (
     <header>
-      <Container>
-        <h1>아이팜</h1>
+      <Container className={styles.container}>
+        <h1 className={styles.logo}>
+          <Link to="/">아이팜</Link>
+        </h1>
         <Nav />
       </Container>
     </header>
--- src/components/layout/header/nav/Nav.js
+++ src/components/layout/header/nav/Nav.js
@@ -1,7 +1,39 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { paths } from '../../../../lib/menu';
+import styles from './Nav.module.scss';
 
-function Nav(props) {
-  return <div></div>;
+function NavLink({ className, path, children }) {
+  return (
+    <li className={className}>
+      <Link to={path}>{children}</Link>
+    </li>
+  );
+}
+
+function Nav() {
+  return (
+    <>
+      <nav className={styles.nav}>
+        <div className={styles.spot}>
+          <ul>
+            {paths.spot.map((menu, idx) => (
+              <NavLink key={idx} path={menu.path}>
+                {menu.name}
+              </NavLink>
+            ))}
+          </ul>
+        </div>
+        <ul className={styles.main}>
+          {paths.gnb.map((menu, idx) => (
+            <NavLink key={idx} path={menu.path}>
+              {menu.name}
+            </NavLink>
+          ))}
+        </ul>
+      </nav>
+    </>
+  );
 }
 
 export default Nav;
