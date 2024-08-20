f3d26ace6176b45c8e0f42bd1f0bec6b61aed2b1
yeeun <48862667+goyangyee22@users.noreply.github.com>
2024-08-20 16:10:51 +0900

    update

--- src/pages/customer-service-center/CustomerServiceCenter.js
+++ src/pages/customer-service-center/CustomerServiceCenter.js
@@ -1,43 +1,20 @@
-import React, { useState } from "react";
+import React from "react";
 import styles from "./CustomerServiceCenter.module.scss";
-import up from "../../../src/assets/arrow/up.png";
-import down from "../../../src/assets/arrow/down.png";
 import RequestForQuote from "./RequestForQuote";
+import Faq from "./Faq";
 
-function CustomerServiceCenter() {
-  // down 버튼을 누르면 description(내용)이 보이고
-  // up 버튼을 누르면 description(내용)이 보이지 않게 됩니다.
-  const [isVisible, setIsVisible] = useState(false);
-
-  // up, down버튼 클릭에 따라 description(내용)표시 여부를 변경합니다.
-  const toggleVisibility = () => {
-    setIsVisible((prevState) => !prevState);
-  };
-
+function CustomerServiceCenter({ id }) {
   return (
-    // <RequestForQuote />
-    <h1>FAQ</h1>
-    // <div className={styles.page}>
-    //   <div className={styles.faq}>
-    //     <div className={styles.title}>
-    //       <h3>tasukete</h3>
-    //       {!isVisible ? (
-    //         <button onClick={toggleVisibility}>
-    //           <img src={down} alt="자세히 보기" />
-    //         </button>
-    //       ) : (
-    //         <button onClick={toggleVisibility}>
-    //           <img src={up} alt="간략히 보기" />
-    //         </button>
-    //       )}
-    //     </div>
-    //     {isVisible && (
-    //       <div className={styles.description}>
-    //         <h4>됬은 없는 단어입니다. 됀은 없는 단어입니다.</h4>
-    //       </div>
-    //     )}
-    //   </div>
-    // </div>
+    <div>
+      <h1>견적요청</h1>
+      <RequestForQuote />
+      <h1>FAQ</h1>
+      <div className={styles.page}>
+        <div>
+          <Faq key={id} />
+        </div>
+      </div>
+    </div>
   );
 }
 
--- src/pages/customer-service-center/Faq.js
+++ src/pages/customer-service-center/Faq.js
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import styles from "./Faq.module.scss";
+import up from "../../../src/assets/arrow/up.png";
+import down from "../../../src/assets/arrow/down.png";
 
 function Faq() {
-  return <div>Faq</div>;
+  // down 버튼을 누르면 description(내용)이 보이고
+  // up 버튼을 누르면 description(내용)이 보이지 않게 됩니다.
+  const [isVisible, setIsVisible] = useState(false);
+
+  // up, down버튼 클릭에 따라 description(내용)표시 여부를 변경합니다.
+  const toggleVisibility = () => {
+    setIsVisible((prevState) => !prevState);
+  };
+  return (
+    <div>
+      <div className={styles.faq}>
+        <div className={styles.title}>
+          <h3>tasukete</h3>
+          {!isVisible ? (
+            <button onClick={toggleVisibility}>
+              <img src={down} alt="자세히 보기" />
+            </button>
+          ) : (
+            <button onClick={toggleVisibility}>
+              <img src={up} alt="간략히 보기" />
+            </button>
+          )}
+        </div>
+        {isVisible && (
+          <div className={styles.description}>
+            <h4>됬은 없는 단어입니다. 됀은 없는 단어입니다.</h4>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default Faq;
