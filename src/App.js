90a2b0f3ad4e270fb3a049798e24627fad4206c3
yeeun <48862667+goyangyee22@users.noreply.github.com>
2024-08-20 13:56:01 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Layout from './components/layout/Layout';
-import LoginPage from './pages/LoginPage/LoginPage';
-import RegisterPage from './pages/RegisterPage/RegisterPage';
-import About from './pages/about/About';
-import Home from './pages/home/Home';
-import Simulation from './pages/simulation/Simulation';
-import Detail from './pages/simulation/details/Detail';
-import './scss/global.scss';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./components/layout/Layout";
+import LoginPage from "./pages/LoginPage/LoginPage";
+import RegisterPage from "./pages/RegisterPage/RegisterPage";
+import About from "./pages/about/About";
+import Home from "./pages/home/Home";
+import Simulation from "./pages/simulation/Simulation";
+import Detail from "./pages/simulation/details/Detail";
+import "./scss/global.scss";
+import CustomerServiceCenter from "./pages/customer-service-center/CustomerServiceCenter";
 
 function App() {
   return (
@@ -21,6 +22,10 @@ function App() {
           </Route>
           <Route path="login" element={<LoginPage />} />
           <Route path="register" element={<RegisterPage />} />
+          <Route
+            path="customer-service-center"
+            element={<CustomerServiceCenter />}
+          />
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ src/pages/customer-service-center/CustomerServiceCenter.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function CustomerServiceCenter() {
+  return <div>고객센터 페이지입니다.</div>;
+}
+
+export default CustomerServiceCenter;
