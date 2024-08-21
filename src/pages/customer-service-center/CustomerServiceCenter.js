054329e6ce49e103d08029d276d8ab5d1369eec8
chajoonho <144306261+yeon7513@users.noreply.github.com>
2024-08-21 12:36:56 +0900

    20240821

--- src/pages/customer-service-center/CustomerServiceCenter.js
+++ src/pages/customer-service-center/CustomerServiceCenter.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import { Link, Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 
 function CustomerServiceCenter() {
   return (
     <div>
-      <Link to="/customer-service-center/request">견적의뢰하기</Link>
-      <Link to="/customer-service-center/faq">FAQ</Link>
       <Outlet />
     </div>
   );
