247770938a3accc326cd9b72cf621762b27064e2
yeeun <48862667+goyangyee22@users.noreply.github.com>
2024-08-21 14:48:17 +0900

    Update Faq.js

--- src/pages/customer-service-center/Faq.js
+++ src/pages/customer-service-center/Faq.js
@@ -67,6 +67,62 @@ function Faq() {
           </div>
         )}
       </div>
+      <div className={styles.faq}>
+        <div className={styles.title}>
+          <h3>대표적인 우리나라 스마트농업 기술을 소개해주세요.</h3>
+          {openId === 3 ? (
+            <button onClick={() => toggleVisibility(3)}>
+              <img src={down} alt="자세히 보기" />
+            </button>
+          ) : (
+            <button onClick={() => toggleVisibility(3)}>
+              <img src={up} alt="간략히 보기" />
+            </button>
+          )}
+        </div>
+        {openId === 3 && (
+          <div className={styles.description}>
+            <h4>
+              3세대 스마트팜 기술을 적용한 스마트 온실 방제 로봇이 있습니다.
+              방제로봇은 8시간 이상 연속 운전이 가능하고, 300L 약액통을 장착해
+              1회 0.33ha를 방제할 수 있습니다. 또한 마그네틱, 근접 센서, 광학
+              검출기 등을 사용해 계획된 경로를 따라 자율주행할 수 있도록
+              설정했습니다. 이와 함께 작업자가 직접 기계를 다루지 않아도 지형
+              환경을 인식해 스스로 주행하며 농작업이 가능한 자율주행 트랙터를
+              비롯해 자율주행 벼 이앙기, 로봇 착유기 국산화 등 스마트농업 기술이
+              개발되었습니다. 현재 방제 로봇은 토마토농장에서 현장실증 중이며,
+              자율주행 트랙터 등은 현장 보급을 위해 노력하고 있습니다.
+            </h4>
+          </div>
+        )}
+      </div>
+      <div className={styles.faq}>
+        <div className={styles.title}>
+          <h3>스마트농업 기술을 도입했을 시 어떤 효과가 있었나요?</h3>
+          {openId === 4 ? (
+            <button onClick={() => toggleVisibility(4)}>
+              <img src={down} alt="자세히 보기" />
+            </button>
+          ) : (
+            <button onClick={() => toggleVisibility(4)}>
+              <img src={up} alt="간략히 보기" />
+            </button>
+          )}
+        </div>
+        {openId === 4 && (
+          <div className={styles.description}>
+            <h4>
+              스마트팜을 설치하여 온실 환경, 생육, 생산량 데이터를 수집한 결과,
+              생산성이 토마토 농장은 13.7%, 딸기 농장은 30% 높아졌습니다. 또한
+              자율주행 벼 이앙기를 사용하면 노동력을 50% 가량 줄일 수 있습니다.
+              드론으로 방제할 경우 노동력은 87%, 농약이 날아서 흩어지는 양은 30%
+              가량 줄어든 것으로 나타났습니다. 그동안 해외에서 수입해 온 로봇
+              착유기는 국산화에 성공함으로써 외국산 대비 설치비를 60% 줄이는
+              효과를 냈습니다.
+            </h4>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
