9a09958f2c19c5ff37d3aede85cbf03c944f6b13
je <wjddmsy12345@gmail.com>
2024-08-23 17:19:06 +0900

    .

--- src/pages/dashboard/DashBoard.js
+++ src/pages/dashboard/DashBoard.js
@@ -1,19 +1,23 @@
 import React from "react";
 import styles from "./DashBoard.module.scss";
 import Img from "../../assets/abou/드론.jpeg";
+import Weather from "./weather/Weather";
+import WeatherElements from "./weather_elements/WeatherElements";
 
 function DashBoard(props) {
   return (
+    // 전체
     <div className={styles.entire}>
       {/* 첫번째라인 */}
       <div className={styles.first_line}>
         {/* 밀양-온습도 강수량 등등 */}
-        <div className={styles.first_one}>
+        <WeatherElements />
+
+        {/* <div className={styles.first_one}>
           <div className={styles.first_one_title}>
             <h1>밀양</h1>
             <button>refresh</button>
           </div>
-          {/*  */}
           <div className={styles.one}>
             <div>
               <h2>온도</h2>
@@ -43,10 +47,12 @@ function DashBoard(props) {
             <h2>월간 강수량</h2>
             <p>132.0</p>
           </div>
-        </div>
+        </div> */}
+
         {/* 날씨 */}
-        <div className={styles.first_two}>
-          <div className={styles.first_two_menu}>
+        <Weather />
+        {/* <div className={styles.first_two}> */}
+        {/* <div className={styles.first_two_menu}>
             <p>일자</p>
             <p>날씨</p>
             <p>강수확률</p>
@@ -94,7 +100,7 @@ function DashBoard(props) {
             <p>00%</p>
             <p>15°C/15°C</p>
           </div>
-        </div>
+        </div> */}
       </div>
 
       {/* 2,3라인 */}
