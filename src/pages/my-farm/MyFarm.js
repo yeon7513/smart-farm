77a61c803b1cf409beff84150255d5282548c385
chajoonho <144306261+yeon7513@users.noreply.github.com>
2024-08-27 09:56:44 +0900

    20240827

--- src/pages/my-farm/MyFarm.js
+++ src/pages/my-farm/MyFarm.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Container from '../../components/layout/container/Container';
 import styles from './MyFarm.module.scss';
 
 function MyFarm() {
   return (
-    <div className={styles.myFarm}>
-      MyFarm
-      <Outlet />
-    </div>
+    <Container className={styles.myFarm}>
+      <div className={styles.map}>지도가 들어갈 예정입니다.</div>
+      <div className={styles.content}>
+        <Outlet />
+      </div>
+    </Container>
   );
 }
 
