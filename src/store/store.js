e5e80ce623789f1861b169781f87a5308f5cc9b3
yeeun <48862667+goyangyee22@users.noreply.github.com>
2024-08-27 17:02:36 +0900

    Update store.js

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import UserSlice from "./user/userSlice";
+import userSlice from "./user/userSlice";
 
 export const store = configureStore({
   reducer: {
-    UserSlice,
+    userSlice,
   },
 });
