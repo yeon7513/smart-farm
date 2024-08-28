eaf2649dd7ac537724e4c995dd40a54ad26fbe34
huiyeon <yeon7513@nate.com>
2024-08-28 19:06:32 +0900

    20240828

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,50 +1,50 @@
-const { createProxyMiddleware } = require("http-proxy-middleware");
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
   // 날씨api
   app.use(
-    "/api", // 기존 host 대신 사용할 경로
+    '/api', // 기존 host 대신 사용할 경로
     createProxyMiddleware({
-      target: "https://apihub.kma.go.kr", // 기존 host
+      target: 'https://apihub.kma.go.kr', // 기존 host
       changeOrigin: true,
       pathRewrite: {
-        "^/api": "",
+        '^/api': '',
       },
     })
   );
   // 병해충api
   app.use(
-    "/api1", // 기존 host 대신 사용할 경로
+    '/api1', // 기존 host 대신 사용할 경로
     createProxyMiddleware({
-      target: "https://ncpms.rda.go.kr/npmsAPI/service", // 기존 host
+      target: 'https://ncpms.rda.go.kr/npmsAPI/service', // 기존 host
       changeOrigin: true,
       pathRewrite: {
-        "^/api1": "",
+        '^/api1': '',
       },
     })
   );
 
   // 우수농가api
   app.use(
-    "/api2", // 기존 host 대신 사용할 경로
+    '/api2', // 기존 host 대신 사용할 경로
     createProxyMiddleware({
-      target: "https://www.smartfarmkorea.net", // 기존 host
+      target: 'https://www.smartfarmkorea.net', // 기존 host
       changeOrigin: true,
       pathRewrite: {
-        "^/api2": "",
+        '^/api2': '',
       },
     })
   );
 
   // 스마트팜 빅데이터api
-  app.use(
-    "/api3", // 기존 host 대신 사용할 경로
-    createProxyMiddleware({
-      target: "https://www.smartfarmkorea.net", // 기존 host
-      changeOrigin: true,
-      pathRewrite: {
-        "^/api3": "",
-      },
-    })
-  );
+  // app.use(
+  //   "/api3", // 기존 host 대신 사용할 경로
+  //   createProxyMiddleware({
+  //     target: "https://www.smartfarmkorea.net", // 기존 host
+  //     changeOrigin: true,
+  //     pathRewrite: {
+  //       "^/api3": "",
+  //     },
+  //   })
+  // );
 };
