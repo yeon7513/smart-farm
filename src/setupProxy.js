22f2c86faa6a795c6619cd79c73b272592a83d02
seasukmin <seh9210@gmail.com>
2024-08-28 19:05:52 +0900

    1

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -40,7 +40,7 @@ module.exports = function (app) {
   app.use(
     "/api3", // 기존 host 대신 사용할 경로
     createProxyMiddleware({
-      target: "https://www.smartfarmkorea.net", // 기존 host
+      target: "", // 기존 host
       changeOrigin: true,
       pathRewrite: {
         "^/api3": "",
