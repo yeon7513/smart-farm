b231f291c14839d205a74ce5c722bfbe88a2b2a8
yeeun <48862667+goyangyee22@users.noreply.github.com>
2024-08-28 10:30:20 +0900

    update

--- src/pages/MyPayment/PaymentsItem.js
+++ src/pages/MyPayment/PaymentsItem.js
@@ -1,7 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-function PaymentsItem() {
-  return <div>PaymentsItem</div>;
+function PaymentsItem({ id }) {
+  return (
+    <li>
+      <Link to={`/payment/${id}`}>테스트</Link>
+      <div>
+        <h4>카테고리</h4>
+        <h3>제목</h3>
+      </div>
+    </li>
+  );
 }
 
 export default PaymentsItem;
