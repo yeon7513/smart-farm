bb9202a2827ff58f50b7d9638be7eee1f4e154a6
yeeun <48862667+goyangyee22@users.noreply.github.com>
2024-08-28 12:28:03 +0900

    update

--- src/pages/request/FacilitiesHorticulture.js
+++ src/pages/request/FacilitiesHorticulture.js
@@ -2,41 +2,61 @@ import React from "react";
 
 const options = {
   "환경 제어": [
-    { id: "thermostat", value: "온도 조절기", label: "온도 조절기" },
-    { id: "ventilationSystem", value: "환기 장치", label: "환기 장치" },
-    { id: "shadingFilm", value: "차광막", label: "차광막" },
+    {
+      id: "thermostat",
+      value: "온도 조절기",
+      label: "온도 조절기: 35,000원",
+    },
+    {
+      id: "ventilationSystem",
+      value: "환기 장치",
+      label: "환기 장치: 201,500원",
+    },
+    { id: "shadingFilm", value: "차광막", label: "차광막: 78,200원" },
   ],
   "조명 시스템": [
-    { id: "LEDGrowLights", value: "인공 조명", label: "인공 조명" },
+    {
+      id: "LEDGrowLights",
+      value: "인공 조명",
+      label: "인공 조명: 186,670원",
+    },
     {
       id: "automaticLightingRegulator",
       value: "자동 조명 조절기",
-      label: "자동 조명 조절기",
+      label: "자동 조명 조절기: 32,400원",
     },
   ],
   "관수 시스템": [
     {
       id: "automaticIrrigationSystem",
       value: "자동 관수 시스템",
-      label: "자동 관수 시스템",
+      label: "자동 관수 시스템: 56,800원",
     },
-    { id: "positiveLiquidMachine", value: "양액기", label: "양액기" },
+    {
+      id: "positiveLiquidMachine",
+      value: "양액기",
+      label: "양액기: 328,500원",
+    },
   ],
   "센서 및 모니터링": [
     {
       id: "temperatureHumiditySensor",
-      value: "온도, 습도 센서",
-      label: "온도 및 습도 센서",
+      value: "온도 및 습도 센서",
+      label: "온도 및 습도 센서: 90,090원",
     },
-    { id: "CO2Sensor", value: "CO2 센서", label: "CO2 센서" },
+    {
+      id: "CO2Sensor",
+      value: "CO2 센서",
+      label: "CO2 센서: 66,000원",
+    },
   ],
   "기타 장비": [
     {
       id: "positiveSolutionMeasurementSensor",
       value: "양액측정센서",
-      label: "양액측정센서",
+      label: "양액측정센서: 569,720원",
     },
-    { id: "CCTV", value: "CCTV", label: "CCTV" },
+    { id: "CCTV", value: "CCTV", label: "CCTV: 39,800원" },
   ],
 };
 
--- src/pages/request/OpenGround.js
+++ src/pages/request/OpenGround.js
@@ -5,36 +5,56 @@ const options = {
     {
       id: "dripIrrigationSystem",
       value: "드립 관수 시스템",
-      label: "드립 관수 시스템",
+      label: "드립 관수 시스템: 288,000원",
     },
     {
       id: "sprinklerSystem",
       value: "스프링클러 시스템",
-      label: "스프링클러 시스템",
+      label: "스프링클러 시스템: 132,000원",
     },
   ],
   "토양 관리": [
-    { id: "soilPhMeter", value: "토양 ph 측정기", label: "토양 ph 측정기" },
+    {
+      id: "soilPhMeter",
+      value: "토양 ph 측정기",
+      label: "토양 ph 측정기: 667,700원",
+    },
     {
       id: "soilHumiditySensor",
       value: "토양 습도 센서",
-      label: "토양 습도 센서",
+      label: "토양 습도 센서: 392,000원",
     },
   ],
   "비료 및 농약 관리": [
     {
       id: "fertilizerApplicationMachine",
       value: "비료 살포기",
-      label: "비료 살포기",
+      label: "비료 살포기: 229,000원",
     },
-    { id: "pesticideSprayer", value: "농약 살포기", label: "농약 살포기" },
+    {
+      id: "pesticideSprayer",
+      value: "농약 살포기",
+      label: "농약 살포기: 242,400원",
+    },
   ],
   "기상 모니터링": [
-    { id: "weatherStation", value: "기상 스테이션", label: "기상 스테이션" },
+    {
+      id: "weatherStation",
+      value: "기상 스테이션",
+      label: "기상 스테이션: 20,000원",
+    },
   ],
   "기타 장비": [
-    { id: "groundBasedDrone", value: "지상용 드론", label: "지상용 드론" },
-    { id: "tractor", value: "트랙터", label: "트랙터" },
+    {
+      id: "groundBasedDrone",
+      value: "지상용 드론",
+      label: "지상용 드론: 41,250,000원",
+    },
+    {
+      id: "tractor",
+      value: "트랙터",
+      label: "트랙터: 16,000,000원",
+    },
   ],
 };
 
