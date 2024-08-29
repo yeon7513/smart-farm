85b89c477a84b801cad84c8d47968be8e48f0c21
seasukmin <seh9210@gmail.com>
2024-08-29 17:32:58 +0900

    240829

--- src/pages/community/Community.js
+++ src/pages/community/Community.js
@@ -1,19 +1,18 @@
 import React from "react";
-import { useLocation } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import PageTemplate from "../../components/layout/page-template/PageTemplate";
-import { getPageLinks, getTitleProps } from "../../utils/pageSetting";
+// import { getPageLinks, getTitleProps } from "../../utils/pageSetting";
 
 function Community() {
-  const { pathname } = useLocation();
-  const titleProps = getTitleProps(pathname);
-  const links = getPageLinks("community");
-
+  //   const { pathname } = useLocation();
+  // const titleProps = getTitleProps(pathname);
+  // const links = getPageLinks("community");
   // return <PageTemplate titleProps={titleProps} components={componentName} />;
-  return (
-    <>
-      <Outlet />
-    </>
-  );
+  // return (
+  //   <>
+  //     <Outlet />
+  //   </>
+  // );
 }
 
 export default Community;
