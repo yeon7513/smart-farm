67cc43d03a9962a6cea22a8ca80e24ff7ca0f002
je <wjddmsy12345@gmail.com>
2024-08-29 09:30:00 +0900

    .

--- src/pages/info/diseases/Diseases.js
+++ src/pages/info/diseases/Diseases.js
@@ -72,13 +72,14 @@ import { Link, Outlet } from "react-router-dom";
 // ];
 
 function Diseases(props) {
-  // const [pests, setPests] = useState(null);
+  const [pests, setPests] = useState(null);
 
   // useEffect(() => {
   //   const apiKey = "2024fae68820b6a8f539fd5def6a6dfd02c1";
   //   const fetchData = async () => {
   //     fetch(
   //       `http://api.rda.go.kr/npmsAPI/service$apiKey=${apiKey}&serviceCode=SVC01&serviceType=AA003&cropName=논벼&sKncrCode1=FC&sKncrCode2=FC01`
+  //       // ` http://ncpms.rda.go.kr/npmsAPI/service?apiKey=${apiKey}&serviceCode=SVC01&serviceType=AA003&dtlSrchFlag=kncr1`
   //     )
   //       .then((response) => response.json())
   //       .then((result) => {
@@ -87,9 +88,33 @@ function Diseases(props) {
   //   };
   //   fetchData();
   // }, []);
+
+  // useEffect(() => {
+  //   const apiKey = "2024fae68820b6a8f539fd5def6a6dfd02c1";
+  //   const fetchData = async () => {
+  //     try {
+  //       const response = await fetch(
+  //         `/api1/apiKey=${apiKey}&serviceCode=SVC01&serviceType=AA003&cropName=논벼&sKncrCode1=FC&sKncrCode2=FC01`
+  //       );
+
+  //       if (!response.ok) {
+  //         throw new Error(`HTTP error! status: ${response.status}`);
+  //       }
+  //       // console.log(response);
+  //       const result = await response.json();
+  //       console.log(result);
+  //       setPests(result);
+  //     } catch (error) {
+  //       console.error("There was a problem with the fetch operation:", error);
+  //     }
+  //   };
+
+  //   fetchData();
+  // }, []);
   return (
     <>
       <div className={styles.main}>
+        {/* {pests && <pre>{JSON.stringify(pests, null, 2)}</pre>} */}
         <div className={styles.list}>
           <Outlet />
         </div>
