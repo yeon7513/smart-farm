12d0f82cb3abbdbacd481334ae939e880fc6ad58
huiyeon <yeon7513@nate.com>
2024-08-29 17:06:06 +0900

    20240829

--- src/App.js
+++ src/App.js
@@ -1,33 +1,33 @@
-import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import Layout from "./components/layout/Layout";
-import { ComponentProvider } from "./context/ComponentContext";
-import LoginPage from "./pages/LoginPage/LoginPage";
-import SearchEm from "./pages/LoginPage/searchEm/SearchEm";
-import SearchPw from "./pages/LoginPage/searchPw/SearchPw";
-import MyPage from "./pages/MyPage/MyPage";
-import RegisterPage from "./pages/RegisterPage/RegisterPage";
-import About from "./pages/about/About";
-import Service from "./pages/about/Service";
-import Community from "./pages/community/Community";
+import Layout from './components/layout/Layout';
+import { ComponentProvider } from './context/ComponentContext';
+import LoginPage from './pages/LoginPage/LoginPage';
+import SearchEm from './pages/LoginPage/searchEm/SearchEm';
+import SearchPw from './pages/LoginPage/searchPw/SearchPw';
+import MyPage from './pages/MyPage/MyPage';
+import RegisterPage from './pages/RegisterPage/RegisterPage';
+import About from './pages/about/About';
+import Service from './pages/about/Service';
+import Community from './pages/community/Community';
 
-import Home from "./pages/home/Home";
-import Info from "./pages/info/Info";
+import Home from './pages/home/Home';
+import Info from './pages/info/Info';
 
-import DiseasesItem from "./pages/info/diseases/diseases-item/DiseasesItem";
+import DiseasesItem from './pages/info/diseases/diseases-item/DiseasesItem';
 
-import PostView from "./components/board/post-view/PostView";
-import CommunityMenu from "./context/CommunityMenu";
-import InfoMenu from "./context/InfoMenu";
-import MyPageMenu from "./context/MyPageMenu";
-import Manager from "./pages/manager/Manager";
-import MyFarm from "./pages/my-farm/MyFarm";
-import FarmList from "./pages/my-farm/my-farm-list/FarmList";
-import DashBoard from "./pages/my-farm/my-farm-list/dashboard/DashBoard";
-import RenderingMenu from "./pages/my-farm/my-farm-list/dashboard/RenderingMenu";
-import RequestForQuote from "./pages/request/RequestForQuote";
-import "./scss/global.scss";
+import PostView from './components/board/post-view/PostView';
+import CommunityMenu from './context/CommunityMenu';
+import InfoMenu from './context/InfoMenu';
+import MyPageMenu from './context/MyPageMenu';
+import Manager from './pages/manager/Manager';
+import MyFarm from './pages/my-farm/MyFarm';
+import FarmList from './pages/my-farm/my-farm-list/FarmList';
+import DashBoard from './pages/my-farm/my-farm-list/dashboard/DashBoard';
+import RenderingMenu from './pages/my-farm/my-farm-list/dashboard/RenderingMenu';
+import RequestForQuote from './pages/request/RequestForQuote';
+import './scss/global.scss';
 
 function App() {
   return (
