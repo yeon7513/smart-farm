9b0cbc010d1216cfd1eab3ce665f256041163f1b
huiyeon <yeon7513@nate.com>
2024-08-30 17:18:49 +0900

    20240830

--- src/pages/LoginPage/Kakaoback.js
+++ src/pages/LoginPage/Kakaoback.js
@@ -1,16 +1,16 @@
-import KakaoLogin from "react-kakao-login";
+import KakaoLogin from 'react-kakao-login';
 
 const Kakaoback = () => {
-  const kakaoClientId = "ab4d55383cde6894b80a6f361124e20b"; // 실제 키로 변경
+  const kakaoClientId = 'ab4d55383cde6894b80a6f361124e20b'; // 실제 키로 변경
 
   const kakaoOnSuccess = async (data) => {
-    console.log("카카오 로그인 성공:", data);
+    console.log('카카오 로그인 성공:', data);
     const idToken = data.response.access_token; // 엑세스 토큰
-    console.log("엑세스 토큰:", idToken);
+    console.log('엑세스 토큰:', idToken);
   };
 
   const kakaoOnFailure = (error) => {
-    console.error("카카오 로그인 실패:", error);
+    console.error('카카오 로그인 실패:', error);
   };
 
   return (
