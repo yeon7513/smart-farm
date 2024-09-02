70ea8e882084496b1cc63902f08fa8238a25884c
seasukmin <seh9210@gmail.com>
2024-09-02 15:52:01 +0900

    240902

--- src/pages/LoginPage/sign-in/SignIn.js
+++ src/pages/LoginPage/sign-in/SignIn.js
@@ -1,13 +1,13 @@
-import { signInWithEmailAndPassword } from 'firebase/auth';
-import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
-import { getUserAuth } from '../../../api/firebase';
-import Form from '../../../components/form/Form';
-import { setUser } from '../../../store/user/UserSlice';
+import { signInWithEmailAndPassword } from "firebase/auth";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { getDatas, getUserAuth } from "../../../api/firebase";
+import Form from "../../../components/form/Form";
+import { setUser } from "../../../store/user/UserSlice";
 
 function SignIn(props) {
-  const [firebaseError, setFirebaseError] = useState('');
+  const [firebaseError, setFirebaseError] = useState("");
   const dispatch = useDispatch();
   const auth = getUserAuth();
   const navigate = useNavigate();
@@ -20,27 +20,32 @@ function SignIn(props) {
         password
       );
       const { user } = userCredential;
-
-      dispatch(
-        setUser({
-          email: user.email,
-          token: user.refreshToken,
-          uid: user.uid,
-          name: user.name,
-          nick: user.nickname,
-        })
+      const userInfo = await getDatas("users");
+      const userInfoConfirm = userInfo.filter(
+        (item) => item.email == user.email
       );
-      navigate('/');
+      userInfoConfirm.forEach((item) => {
+        dispatch(
+          setUser({
+            email: user.email,
+            token: user.refreshToken,
+            uid: user.uid,
+            name: item.name,
+            nick: item.nickname,
+          })
+        );
+      });
+      navigate("/");
     } catch (error) {
       console.log(error);
-      setFirebaseError('이메일 또는 비밀번호가 틀렸습니다.');
+      setFirebaseError("이메일 또는 비밀번호가 틀렸습니다.");
     }
   };
   return (
     <Form
-      title={'로그인'}
-      placeholder1={'Email'}
-      placeholder2={'Password'}
+      title={"로그인"}
+      placeholder1={"Email"}
+      placeholder2={"Password"}
       getDataForm={handleLogin}
       firebaseError={firebaseError}
     />
