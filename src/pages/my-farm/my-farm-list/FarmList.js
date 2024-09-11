20c1b6b5cf8487995f00f9f07d4e00d4a8b1dc38
je <wjddmsy12345@gmail.com>
2024-09-11 17:08:17 +0900

    .

--- src/pages/my-farm/my-farm-list/FarmList.js
+++ src/pages/my-farm/my-farm-list/FarmList.js
@@ -1,27 +1,28 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchCommonInfo } from '../../../store/dashboard/dashboardSlice';
-import styles from './FarmList.module.scss';
-import FarmListItem from './my-farm-list-item/FarmListItem';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchCommonInfo } from "../../../store/dashboard/dashboardSlice";
+import styles from "./FarmList.module.scss";
+import FarmListItem from "./my-farm-list-item/FarmListItem";
 
 function FarmList() {
   const { commonInfo } = useSelector((state) => state.dashboardSlice);
 
   const dispatch = useDispatch();
 
-  const userEmail = JSON.parse(localStorage.getItem('user')).email;
+  // const userEmail = JSON.parse(localStorage.getItem('user')).email;
 
-  const list = userEmail.includes('admin')
-    ? commonInfo
-    : commonInfo.filter(
-        (list) =>
-          list.userId === userEmail &&
-          list.useYn === 'Y' &&
-          list.deleteYn === 'N'
-      );
+  const list = commonInfo;
+  //  userEmail.includes("admin")
+  //   ? commonInfo
+  //   : commonInfo.filter(
+  //       (list) =>
+  //         list.userId === userEmail &&
+  //         list.useYn === "Y" &&
+  //         list.deleteYn === "N"
+  //     );
 
   useEffect(() => {
-    dispatch(fetchCommonInfo('dashboard'));
+    dispatch(fetchCommonInfo("dashboard"));
   }, [dispatch]);
 
   return (
