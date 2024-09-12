ac4e8fa99e9b02512d5302993e68acf319ba45e6
jhlee <48862667+goyangyee22@users.noreply.github.com>
2024-09-12 13:43:14 +0900

    update

--- src/store/faq-data/faqDataSlice.js
+++ src/store/faq-data/faqDataSlice.js
@@ -1,5 +1,5 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import { db, getOrder } from "../../api/firebase";
+import { db } from "../../api/firebase";
 import { collection, getDocs, orderBy, query } from "firebase/firestore";
 
 export const fetchfaqData = createAsyncThunk(
@@ -12,15 +12,6 @@ export const fetchfaqData = createAsyncThunk(
     const querySnapshot = await getDocs(q);
     return querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
   }
-  // async ({ collectionName, orderByField }) => {
-  //   try {
-  //     const resultData = await getOrder(collectionName, orderByField);
-  //     return resultData;
-  //   } catch (error) {
-  //     console.error(error);
-  //     throw error;
-  //   }
-  // }
 );
 
 const faqDataSlice = createSlice({
@@ -30,25 +21,6 @@ const faqDataSlice = createSlice({
     setFaqData: (state, action) => {
       return action.payload;
     },
-    // updateFaq: (state, action) => {
-    //   return state.map((faq) =>
-    //     faq.id === action.payload.id ? action.payload : faq
-    //   );
-    // },
-    // setLike: (state, action) => {
-    //   return state.map((faq) =>
-    //     faq.id === action.payload.id
-    //       ? { ...faq, liked: action.payload.liked, likes: action.payload.likes }
-    //       : faq
-    //   );
-    // },
-    // removeLike: (state, action) => {
-    //   return state.map((faq) =>
-    //     faq.id === action.payload.id
-    //       ? { ...faq, liked: action.payload.liked, likes: action.payload.likes }
-    //       : faq
-    //   );
-    // },
   },
   extraReducers: (builder) => {
     builder
