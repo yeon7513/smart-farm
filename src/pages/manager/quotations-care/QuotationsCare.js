1bf5bbcdbcf79814f2e84afbbeda5accb610c3a7
jhlee <48862667+goyangyee22@users.noreply.github.com>
2024-09-13 16:18:58 +0900

    Update QuotationsCare.js

--- src/pages/manager/quotations-care/QuotationsCare.js
+++ src/pages/manager/quotations-care/QuotationsCare.js
@@ -1,12 +1,12 @@
-import { saveAs } from 'file-saver';
-import React, { useEffect } from 'react';
-import { TbPencilSearch } from 'react-icons/tb';
-import { useDispatch, useSelector } from 'react-redux';
-import { BeatLoader } from 'react-spinners';
-import * as XLSX from 'xlsx';
-import SearchBox from '../../../components/search_box/SearchBox';
-import { fetchPayments } from '../../../store/payment/paymentsSlice';
-import styles from './QuotationsCare.module.scss';
+import { saveAs } from "file-saver";
+import React, { useEffect } from "react";
+import { TbPencilSearch } from "react-icons/tb";
+import { useDispatch, useSelector } from "react-redux";
+import { BeatLoader } from "react-spinners";
+import * as XLSX from "xlsx";
+import SearchBox from "../../../components/search_box/SearchBox";
+import { fetchPayments } from "../../../store/payment/paymentsSlice";
+import styles from "./QuotationsCare.module.scss";
 
 function QuotationsCare() {
   const { payments, isLoading } = useSelector((state) => state.paymentsSlice);
@@ -14,7 +14,7 @@ function QuotationsCare() {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(fetchPayments('users'));
+    dispatch(fetchPayments("users"));
   }, [dispatch, payments]);
 
   // firebase의 데이터를 excel로 불러옵니다.
@@ -22,19 +22,19 @@ function QuotationsCare() {
     // 데이터 변환
     const worksheet = XLSX.utils.json_to_sheet(payments);
     const workbook = XLSX.utils.book_new();
-    XLSX.utils.book_append_sheet(workbook, worksheet, '결제 내역');
+    XLSX.utils.book_append_sheet(workbook, worksheet, "결제 내역");
 
     // 엑셀 파일 생성
     const excelBuffer = XLSX.write(workbook, {
-      bookType: 'xlsx',
-      type: 'array',
+      bookType: "xlsx",
+      type: "array",
     });
     const file = new Blob([excelBuffer], {
-      type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+      type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
     });
 
     // 파일 다운로드
-    saveAs(file, '결제 내역.xlsx');
+    saveAs(file, "결제 내역.xlsx");
   };
   return (
     <div className={styles.quotations}>
@@ -44,7 +44,7 @@ function QuotationsCare() {
         <>
           <SearchBox
             name={<TbPencilSearch />}
-            placeholder={'견적 의뢰서 검색'}
+            placeholder={"견적 의뢰서 검색"}
           />
           <button onClick={exportToExcel}>견적 내역 다운로드</button>
           <div>
