0e54f98ca77459b8c261e5c5298a14888d838552
huiyeon <yeon7513@nate.com>
2024-09-20 17:56:13 +0900

    20240920

--- src/components/Mypage/sidebar/Sidebar.js
+++ src/components/Mypage/sidebar/Sidebar.js
@@ -1,11 +1,11 @@
-import React, { useEffect, useState } from "react";
-import { deleteDatas, getUserAuth, updateDatas } from "../../../api/firebase";
-import { useComponentContext } from "../../../context/ComponentContext";
-import styles from "./sidebar.module.scss";
-import { useNavigate } from "react-router-dom";
-import CustomModal from "../../modal/CustomModal";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchItems, removeUser } from "../../../store/user/UserSlice";
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { getUserAuth, updateDatas } from '../../../api/firebase';
+import { useComponentContext } from '../../../context/ComponentContext';
+import { fetchItems, removeUser } from '../../../store/user/UserSlice';
+import CustomModal from '../../modal/CustomModal';
+import styles from './sidebar.module.scss';
 
 function Sidebar(props) {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -14,11 +14,11 @@ function Sidebar(props) {
   const auth = getUserAuth();
   const dispatch = useDispatch();
   const { items } = useSelector((state) => state.userSlice);
-  const user = JSON.parse(localStorage.getItem("user"));
+  const user = JSON.parse(localStorage.getItem('user'));
   const navigate = useNavigate();
   useEffect(() => {
     const localInfoNum = async () => {
-      const localInfo = localStorage.getItem("user");
+      const localInfo = localStorage.getItem('user');
       if (localInfo === null) {
         navigate(-1);
       }
@@ -26,12 +26,12 @@ function Sidebar(props) {
     localInfoNum();
   }, [navigate]);
   useEffect(() => {
-    dispatch(fetchItems({ collectionName: "users" }));
+    dispatch(fetchItems({ collectionName: 'users' }));
   }, []);
   const { currComp, setCurrComp } = useComponentContext();
-  const localInfo = localStorage.getItem("user");
+  const localInfo = localStorage.getItem('user');
   if (!localInfo == null) {
-    navigate("/");
+    navigate('/');
   }
 
   const handleLogout = async () => {
@@ -39,17 +39,17 @@ function Sidebar(props) {
       await auth.signOut();
       dispatch(removeUser());
       if (window.Kakao.Auth.getAccessToken()) {
-        console.log("로그아웃 중입니다.");
+        console.log('로그아웃 중입니다.');
         await new Promise((resolve) => {
           window.Kakao.Auth.logout(function () {
-            console.log("로그아웃 성공");
+            console.log('로그아웃 성공');
             resolve();
           });
         });
       } else {
-        console.log("로그인 상태가 아닙니다.");
+        console.log('로그인 상태가 아닙니다.');
       }
-      navigate("/");
+      navigate('/');
     } catch (error) {
       console.error(error);
     }
@@ -60,47 +60,47 @@ function Sidebar(props) {
     if (deleteUser) {
       const { docId } = deleteUser;
       const updateData = {
-        deleteYn: "Y",
+        deleteYn: 'Y',
       };
-      await updateDatas("users", docId, updateData);
+      await updateDatas('users', docId, updateData);
     } else {
-      console.error("User not found in items");
+      console.error('User not found in items');
     }
     closeModal();
-    alert("탈퇴가 완료되었습니다.");
+    alert('탈퇴가 완료되었습니다.');
     handleLogout();
-    navigate("/");
+    navigate('/');
   };
   return (
     <div className={styles.container}>
       <ul className={styles.items}>
         내 정보
         <li className={styles.item}>
-          <button onClick={() => setCurrComp("Myinfo")}>내 정보 수정</button>
+          <button onClick={() => setCurrComp('Myinfo')}>내 정보 수정</button>
         </li>
         <li className={styles.itemmove}>
-          <button onClick={() => setCurrComp("my-farm")}>내 농장 관리</button>
+          <button onClick={() => navigate('/my-farm')}>내 농장 관리</button>
         </li>
       </ul>
       <ul className={styles.items}>
         견적 및 결제
         <li className={styles.item}>
-          <button onClick={() => setCurrComp("Paymentinfo")}>견적 의뢰</button>
+          <button onClick={() => setCurrComp('Paymentinfo')}>견적 의뢰</button>
         </li>
         <li>
-          <button onClick={() => setCurrComp("Paymentinfo")}>결제 내역</button>
+          <button onClick={() => setCurrComp('Paymentinfo')}>결제 내역</button>
         </li>
       </ul>
       <ul className={styles.items}>
         문의 내역
         <li className={styles.item}>
-          <button onClick={() => setCurrComp("Chatbotinfo")}>챗봇 상담</button>
+          <button onClick={() => setCurrComp('Chatbotinfo')}>챗봇 상담</button>
         </li>
         <li className={styles.item}>
-          <button onClick={() => setCurrComp("Asinfo")}>A/S 문의</button>
+          <button onClick={() => setCurrComp('Asinfo')}>A/S 문의</button>
         </li>
         <li className={styles.item}>
-          <button onClick={() => setCurrComp("Myletter")}>내가 쓴 글</button>
+          <button onClick={() => setCurrComp('Myletter')}>내가 쓴 글</button>
         </li>
       </ul>
       <ul>
@@ -111,8 +111,8 @@ function Sidebar(props) {
         </li>
       </ul>
       <CustomModal
-        title={"회원탈퇴"}
-        btnName={"탈퇴하기"}
+        title={'회원탈퇴'}
+        btnName={'탈퇴하기'}
         isOpen={isModalOpen}
         handleClose={closeModal}
         btnHandler={deleteUserInfo}
