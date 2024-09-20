1692eb1c0859b091e0b77ca1bfe18698b9bc1411
huiyeon <yeon7513@nate.com>
2024-09-20 09:23:36 +0900

    20240920

---
+++ src/api/geoCode.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+const API_KEY = process.env.REACT_APP_GOOGLE_MAP_API_KEY;
+
+export const convertingAddressToGeoCode = async (addr) => {
+  const encodedAddress = encodeURIComponent(addr);
+  const URL = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=${API_KEY}`;
+
+  try {
+    const response = await axios.get(URL);
+    if (response.data.status === 'OK') {
+      const location = response.data.results[0].geometry.location;
+      return { lat: location.lat, lng: location.lng };
+    } else {
+      console.error('좌표 변환 실패: ', response.status);
+    }
+  } catch (error) {
+    console.error('Geocoding API 호출 실패: ', error);
+  }
+};
