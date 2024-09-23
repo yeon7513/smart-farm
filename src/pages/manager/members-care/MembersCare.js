4dc811da9abfe7876dcb10aa44f41dc776f6e1fc
huiyeon <yeon7513@nate.com>
2024-09-23 11:02:04 +0900

    20240923

--- src/pages/manager/members-care/MembersCare.js
+++ src/pages/manager/members-care/MembersCare.js
@@ -6,17 +6,24 @@ import { fetchItems } from '../../../store/user/UserSlice';
 import CustomModal from './../../../components/modal/CustomModal';
 import styles from './MembersCare.module.scss';
 import MemberList from './member-list/MemberList';
+import MemberListEdit from './member-list/member-list-edit/MemberListEdit';
 import MemberListItem from './member-list/member-list-item/MemberListItem';
 
 function MembersCare() {
   const [isOpen, setIsOpen] = useState(false);
   const [userDetail, setUserDetail] = useState();
+  const [isEdit, setIsEdit] = useState(false);
 
   const { items } = useSelector((state) => state.userSlice);
   const dispatch = useDispatch();
 
   const handleClose = () => {
+    setIsEdit(false);
     setIsOpen(false);
+  };
+
+  const handleEdit = () => {
+    setIsEdit(true);
   };
 
   // 회원 목록에서 관리자 제외
@@ -49,9 +56,23 @@ function MembersCare() {
         <CustomModal
           isOpen={isOpen}
           handleClose={handleClose}
-          title={<>{userDetail.name} 님 상세 정보</>}
+          title={
+            !isEdit ? (
+              <>{userDetail.name} 님 상세 정보</>
+            ) : (
+              <>{userDetail.name} 님 정보 수정</>
+            )
+          }
         >
-          <MemberListItem detail={userDetail} />
+          <>
+            {!isEdit ? (
+              <>
+                <MemberListItem detail={userDetail} handleEdit={handleEdit} />
+              </>
+            ) : (
+              <MemberListEdit detail={userDetail} cancelEdit={setIsEdit} />
+            )}
+          </>
         </CustomModal>
       )}
     </>
