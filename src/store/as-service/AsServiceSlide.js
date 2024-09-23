7e18cb6ec4430893058583bed943ff8c34e60d3f
yujin <opgrhd654@gmail.com>
2024-09-23 16:54:57 +0900

    yujin

--- src/store/as-service/AsServiceSlide.js
+++ src/store/as-service/AsServiceSlide.js
@@ -1,8 +1,10 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { getBoardDatas } from "../../api/board";
+import { getDatas } from "../../api/firebase";
 
 const initialState = {
-  posts: [],
+  completing: [],
+  completed: [],
   isLoading: false,
   error: null,
 };
@@ -10,44 +12,89 @@ const initialState = {
 const asSlice = createSlice({
   name: "as",
   initialState,
-  reducers: {
-    // setPosts: (state, action) => {
-    //   state.posts = action.payload;
-    // },
-    // updatePostStatus: (state, action) => {
-    //   const { postId, status } = action.payload;
-    //   const post = state.posts.find((post) => post.id === postId);
-    //   if (post) {
-    //     post.status = status;
-    //   }
-    // },
-  },
+  reducers: {},
   extraReducers: (builder) => {
     builder
-      .addCase(fetchAsDatas.pending, (state) => {
+      //   .addCase(fetchAsDatas.pending, (state) => {
+      //     state.isLoading = true;
+      //     state.error = null;
+      //   })
+      //   .addCase(fetchAsDatas.fulfilled, (state, action) => {
+      //     state.isLoading = false;
+      //     state.posts = action.payload;
+      //     state.error = null;
+      //   })
+      //   .addCase(fetchAsDatas.rejected, (state, action) => {
+      //     state.isLoading = false;
+      //     state.error = action.payload;
+      //   })
+
+      // 답변 중
+      .addCase(fetchCompleting.pending, (state) => {
         state.isLoading = true;
         state.error = null;
       })
-      .addCase(fetchAsDatas.fulfilled, (state, action) => {
+      .addCase(fetchCompleting.fulfilled, (state, action) => {
         state.isLoading = false;
-        state.posts = action.payload;
+        state.completing = action.payload;
+      })
+      .addCase(fetchCompleting.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.payload;
+      })
+
+      // 답변 완료
+      .addCase(fetchCompleted.pending, (state) => {
+        state.isLoading = true;
         state.error = null;
       })
-      .addCase(fetchAsDatas.rejected, (state, action) => {
+      .addCase(fetchCompleted.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.completed = action.payload;
+      })
+      .addCase(fetchCompleted.rejected, (state, action) => {
         state.isLoading = false;
         state.error = action.payload;
       });
   },
 });
 
-export const fetchAsDatas = createAsyncThunk(
-  "board/fetchBoardDatas",
-  async (category) => {
+// export const fetchAsDatas = createAsyncThunk(
+//   "as/fetchAsDatas",
+//   async (category) => {
+//     try {
+//       const data = await getBoardDatas(category);
+//       return data;
+//     } catch (error) {
+//       console.log("as 데이터 불러오기 중 에러: ", error);
+//     }
+//   }
+// );
+
+// 답변 중 불러오기
+export const fetchCompleting = createAsyncThunk(
+  "as/fetchCompleting",
+  async () => {
     try {
-      const data = await getBoardDatas("as");
-      return data;
+      const data = await getDatas("as"); // Firestore에서 모든 데이터를 불러옴
+      const resultData = data.filter((item) => item.completedYn === "n"); // processYn 필드로 필터링
+      return resultData;
     } catch (error) {
-      return error;
+      console.log("답변 중 데이터 불러오기 중 에러: ", error);
+    }
+  }
+);
+
+// 답변 완료 불러오기
+export const fetchCompleted = createAsyncThunk(
+  "as/fetchCompleted",
+  async () => {
+    try {
+      const data = await getDatas("as");
+      const resultData = data.filter((item) => item.completedYn === "Y"); // processYn 필드로 필터링
+      return resultData;
+    } catch (error) {
+      console.log("답변 완료 데이터 불러오기 중 에러: ", error);
     }
   }
 );
