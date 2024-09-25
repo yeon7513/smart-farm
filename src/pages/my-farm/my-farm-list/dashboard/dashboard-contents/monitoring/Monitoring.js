5a104f438cb09b32929e955c07089d002b885512
seasukmin <seh9210@gmail.com>
2024-09-25 12:30:03 +0900

    240925

--- src/pages/my-farm/my-farm-list/dashboard/dashboard-contents/monitoring/Monitoring.js
+++ src/pages/my-farm/my-farm-list/dashboard/dashboard-contents/monitoring/Monitoring.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import { useSectorContext } from "../../../../../../context/SectorContext";
+import { transformDataForBarGraphs } from "./../../../../../../components/chart/Charts";
 
 function Monitoring() {
   const { sector } = useSectorContext();
-
   return (
     <div>
       <span>Monitoring</span>
+
       <Outlet />
     </div>
   );
