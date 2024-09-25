1948b1272fecfb85d20af22a57d750e709ddc9b8
seasukmin <seh9210@gmail.com>
2024-09-25 17:09:02 +0900

    12

--- src/pages/my-farm/my-farm-list/dashboard/dashboard-contents/DashboardContent.js
+++ src/pages/my-farm/my-farm-list/dashboard/dashboard-contents/DashboardContent.js
@@ -22,32 +22,40 @@ function DashboardContent({ docId }) {
     dispatch(fetchSectorInfo(collectionName));
   }, [dispatch, docId]);
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCount((prevCount) => {
-        if (prevCount >= 3000) {
-          return 0;
-        }
-        return prevCount + 0.1;
-      });
-    }, 1000); // Update every second
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     setCount((prevCount) => {
+  //       if (prevCount >= 50) {
+  //         return 0;
+  //       }
+  //       return prevCount + 1;
+  //     });
+  //   }, 1000);
+  //   return () => clearInterval(interval);
+  // }, []);
 
-    return () => clearInterval(interval);
-  }, []);
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     setRandomCount((prevCount) => {
+  //       // 1시간 마다 1000으로 설정
+  //       const currentTime = new Date().getMinutes(); // 현재 시간을 분 단위로 체크
+  //       if (currentTime === 0) {
+  //         // 매시간 정각에 1000 설정
+  //         return 1000;
+  //       }
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setRandomCount((prevCount) => {
-        if (prevCount >= 1000) {
-          return 0;
-        }
-        const randomIncrease = Math.random() * 1000;
-        return prevCount + randomIncrease;
-      });
-    }, 1000);
+  //       // 1000 미만일 때는 랜덤 증가
+  //       if (prevCount >= 1000) {
+  //         return 0;
+  //       }
 
-    return () => clearInterval(interval);
-  }, []);
+  //       const randomIncrease = Math.random() * 1000;
+  //       return randomIncrease;
+  //     });
+  //   }, 10000); // 10초마다 실행
+
+  //   return () => clearInterval(interval);
+  // }, []);
 
   useEffect(() => {
     dispatch(countData(count));
