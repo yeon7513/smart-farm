a949c1941593a4273fb94eb227be5c742a4e0258
huiyeon <yeon7513@nate.com>
2024-09-25 14:33:40 +0900

    20240925

--- src/pages/manager/members-care/MembersCare.js
+++ src/pages/manager/members-care/MembersCare.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { TbUserSearch } from 'react-icons/tb';
 import { useDispatch, useSelector } from 'react-redux';
+import PaginationButton from '../../../components/pagination-button/PaginationButton';
 import SearchBox from '../../../components/search_box/SearchBox';
 import { fetchItems } from '../../../store/user/UserSlice';
 import CustomModal from './../../../components/modal/CustomModal';
@@ -9,13 +10,25 @@ import MemberList from './member-list/MemberList';
 import MemberListEdit from './member-list/member-list-edit/MemberListEdit';
 import MemberListItem from './member-list/member-list-item/MemberListItem';
 
+const PAGE_SIZE_DESKTOP = 6;
+const PAGE_SIZE_MOBILE = 4;
+
 function MembersCare() {
   const [isOpen, setIsOpen] = useState(false);
   const [userDetail, setUserDetail] = useState();
   const [isEdit, setIsEdit] = useState(false);
 
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPage, setTotalPage] = useState(1);
+  const [pageSize, setPageSize] = useState(getPageSize());
+
   const { items } = useSelector((state) => state.userSlice);
   const dispatch = useDispatch();
+
+  // 현제 페이지 사이즈
+  function getPageSize() {
+    return window.innerWidth >= 992 ? PAGE_SIZE_DESKTOP : PAGE_SIZE_MOBILE;
+  }
 
   const handleClose = () => {
     setIsEdit(false);
@@ -26,12 +39,48 @@ function MembersCare() {
     setIsEdit(true);
   };
 
-  // 회원 목록에서 관리자 제외
-  const filteredUsers = items.filter((user) => !user.email.includes('admin'));
+  const filteredUsers = items
+    .filter((user) => !user.email.includes('admin'))
+    .sort((a, b) => b.createdAt - a.createdAt);
 
+  // 관리자를 제외한 회원 목록
+  const getCurrentUsers = () => {
+    const startIndex = (currentPage - 1) * pageSize;
+    return filteredUsers.slice(startIndex, startIndex + pageSize);
+  };
+
+  // 페이지 변경 핸들러
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+  // 윈도우 크기 변경 핸들러
+  const handleResize = () => {
+    const newSize = getPageSize();
+    if (newSize !== pageSize) {
+      setPageSize(newSize);
+      setCurrentPage(1);
+    }
+  };
+
+  // users 컬렉션 전체 불러오기
   useEffect(() => {
     dispatch(fetchItems({ collectionName: 'users' }));
   }, [dispatch]);
+
+  useEffect(() => {
+    const totalPages = Math.ceil(filteredUsers.length / pageSize);
+    setTotalPage(totalPages);
+  }, [filteredUsers, pageSize]);
+
+  // 윈도우 크기 변경 이벤트 리스너 등록 및 삭제
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   return (
     <>
@@ -41,8 +90,7 @@ function MembersCare() {
         placeholder={'회원 검색'}
       />
       <ul className={styles.members}>
-        {filteredUsers // createdAt으로 오름차순 => 신규 가입자가 위로 옵니다.
-          .sort((a, b) => b.createdAt - a.createdAt)
+        {getCurrentUsers() // createdAt으로 오름차순 => 신규 가입자가 위로 옵니다.
           .map((item, idx) => (
             <MemberList
               key={idx}
@@ -52,6 +100,25 @@ function MembersCare() {
             />
           ))}
       </ul>
+      <div className={styles.pagination}>
+        <button
+          onClick={() => handlePageChange(1)}
+          disabled={currentPage === 1}
+        >
+          처음
+        </button>
+        <PaginationButton
+          currentPage={currentPage}
+          totalPage={totalPage}
+          onPageChange={handlePageChange}
+        />
+        <button
+          onClick={() => handlePageChange(totalPage)}
+          disabled={currentPage === totalPage}
+        >
+          끝
+        </button>
+      </div>
       {isOpen && (
         <CustomModal
           isOpen={isOpen}
