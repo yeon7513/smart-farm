cd82da645f8b44627ed776a496345d3d3b294660
JuWon <yjw1732@gmail.com>
2024-09-26 10:18:55 +0900

    ...

--- src/pages/manager/chat-room-care/ChatRoomCare.js
+++ src/pages/manager/chat-room-care/ChatRoomCare.js
@@ -1,56 +1,19 @@
 import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux'; // 중복된 useDispatch import 제거
-import { setMessages } from '../../../store/chat-room/chatRoomSlice';
-import { collection, query, orderBy, onSnapshot } from 'firebase/firestore';
-import { db, fetchChatroomId } from '../../../api/firebase';
+import { TbMessageSearch } from 'react-icons/tb';
+import SearchBox from '../../../components/search_box/SearchBox';
 import styles from './ChatRoomCare.module.scss';
 
 
-function ChatRoomCare({ userEmail }) {
-  const dispatch = useDispatch();
-  const chatroomId = useSelector((state) => state.chatRoom.chatroomId); // Redux에서 chatroomId 가져오기
-
-  useEffect(() => {
-    if (!chatroomId) return;
-
-    const q = query(
-      collection(db, 'chatbot', userEmail, 'chatroom1', chatroomId, 'message'),
-      orderBy('createdAt', 'asc')
-    );
-
-    const unsubscribe = onSnapshot(q, (snapshot) => {
-      const fetchedMessages = snapshot.docs.map((doc) => ({
-        id: doc.id,
-        ...doc.data(),
-      }));
-      dispatch(setMessages(fetchedMessages)); // 상태 업데이트
-    });
-
-    return () => unsubscribe();
-  }, [chatroomId, userEmail, dispatch]);
-
-  // Firestore에서 chatroomId 가져오기
-  useEffect(() => {
-    async function exampleUsage() {
-      const email = 'yjw1732@gmail.com'; // 예시 이메일
-      const chatroomId = await fetchChatroomId(email);
-    
-      if (chatroomId) {
-        console.log('Fetched chatroomId:', chatroomId);
-        // chatroomId로 Firestore에서 해당 방의 데이터를 처리하는 로직 추가 가능
-      } else {
-        console.log('No chatroom found for this user.');
-      }
-    }
-
-    exampleUsage();
-  }, [dispatch]);
+function ChatRoomCare() {
 
   return (
     <div className={styles.chatRoom}>
-      {/* ChatRequestList 렌더링 */}
-    </div>
+     <SearchBox name={<TbMessageSearch />} placeholder={'채팅방 검색'} />
+ChatroomCare
+</div>
+ 
   );
 }
+
 
 export default ChatRoomCare;
