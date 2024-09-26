f3c138bea98e55b46bb94d607a70b3258b08c8bf
huiyeon <yeon7513@nate.com>
2024-09-26 10:27:23 +0900

    20240926

--- src/pages/my-farm/my-farm-list/dashboard/dashboard-contents/monitoring/Monitoring.js
+++ src/pages/my-farm/my-farm-list/dashboard/dashboard-contents/monitoring/Monitoring.js
@@ -1,8 +1,7 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
-import { useSectorContext } from "../../../../../../context/SectorContext";
-import SimpleLineChart from "../../../../../../components/chart/SimpleLineChart";
-import { useSelector } from "react-redux";
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Outlet } from 'react-router-dom';
+import { useSectorContext } from '../../../../../../context/SectorContext';
 
 function Monitoring() {
   const { growthData } = useSelector((state) => state.bestfarmSlice);
@@ -14,6 +13,97 @@ function Monitoring() {
       <span>Monitoring</span>
       {/* <SimpleLineChart /> */}
       <Outlet />
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut vitae
+        reiciendis perferendis a libero ipsum, porro possimus ullam saepe vel!
+        Quos, nesciunt architecto. Nam ipsum, laboriosam amet veritatis
+        dignissimos explicabo quasi vel. Odit et odio possimus, officia itaque
+        est ipsa obcaecati laborum. Modi expedita optio, maxime animi eum
+        beatae, ratione in ad aut debitis autem voluptatum officiis voluptatibus
+        amet corporis consectetur sapiente, nihil libero. Pariatur doloremque,
+        recusandae commodi totam reprehenderit libero quas fuga odit voluptas
+        vero aliquid ducimus non distinctio ea repudiandae deleniti et earum
+        dolore? Expedita adipisci molestiae obcaecati officiis accusamus
+        dolorem, autem tempora iure maiores saepe cumque harum.
+      </div>
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut vitae
+        reiciendis perferendis a libero ipsum, porro possimus ullam saepe vel!
+        Quos, nesciunt architecto. Nam ipsum, laboriosam amet veritatis
+        dignissimos explicabo quasi vel. Odit et odio possimus, officia itaque
+        est ipsa obcaecati laborum. Modi expedita optio, maxime animi eum
+        beatae, ratione in ad aut debitis autem voluptatum officiis voluptatibus
+        amet corporis consectetur sapiente, nihil libero. Pariatur doloremque,
+        recusandae commodi totam reprehenderit libero quas fuga odit voluptas
+        vero aliquid ducimus non distinctio ea repudiandae deleniti et earum
+        dolore? Expedita adipisci molestiae obcaecati officiis accusamus
+        dolorem, autem tempora iure maiores saepe cumque harum.
+      </div>
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut vitae
+        reiciendis perferendis a libero ipsum, porro possimus ullam saepe vel!
+        Quos, nesciunt architecto. Nam ipsum, laboriosam amet veritatis
+        dignissimos explicabo quasi vel. Odit et odio possimus, officia itaque
+        est ipsa obcaecati laborum. Modi expedita optio, maxime animi eum
+        beatae, ratione in ad aut debitis autem voluptatum officiis voluptatibus
+        amet corporis consectetur sapiente, nihil libero. Pariatur doloremque,
+        recusandae commodi totam reprehenderit libero quas fuga odit voluptas
+        vero aliquid ducimus non distinctio ea repudiandae deleniti et earum
+        dolore? Expedita adipisci molestiae obcaecati officiis accusamus
+        dolorem, autem tempora iure maiores saepe cumque harum.
+      </div>
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut vitae
+        reiciendis perferendis a libero ipsum, porro possimus ullam saepe vel!
+        Quos, nesciunt architecto. Nam ipsum, laboriosam amet veritatis
+        dignissimos explicabo quasi vel. Odit et odio possimus, officia itaque
+        est ipsa obcaecati laborum. Modi expedita optio, maxime animi eum
+        beatae, ratione in ad aut debitis autem voluptatum officiis voluptatibus
+        amet corporis consectetur sapiente, nihil libero. Pariatur doloremque,
+        recusandae commodi totam reprehenderit libero quas fuga odit voluptas
+        vero aliquid ducimus non distinctio ea repudiandae deleniti et earum
+        dolore? Expedita adipisci molestiae obcaecati officiis accusamus
+        dolorem, autem tempora iure maiores saepe cumque harum.
+      </div>
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut vitae
+        reiciendis perferendis a libero ipsum, porro possimus ullam saepe vel!
+        Quos, nesciunt architecto. Nam ipsum, laboriosam amet veritatis
+        dignissimos explicabo quasi vel. Odit et odio possimus, officia itaque
+        est ipsa obcaecati laborum. Modi expedita optio, maxime animi eum
+        beatae, ratione in ad aut debitis autem voluptatum officiis voluptatibus
+        amet corporis consectetur sapiente, nihil libero. Pariatur doloremque,
+        recusandae commodi totam reprehenderit libero quas fuga odit voluptas
+        vero aliquid ducimus non distinctio ea repudiandae deleniti et earum
+        dolore? Expedita adipisci molestiae obcaecati officiis accusamus
+        dolorem, autem tempora iure maiores saepe cumque harum.
+      </div>
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut vitae
+        reiciendis perferendis a libero ipsum, porro possimus ullam saepe vel!
+        Quos, nesciunt architecto. Nam ipsum, laboriosam amet veritatis
+        dignissimos explicabo quasi vel. Odit et odio possimus, officia itaque
+        est ipsa obcaecati laborum. Modi expedita optio, maxime animi eum
+        beatae, ratione in ad aut debitis autem voluptatum officiis voluptatibus
+        amet corporis consectetur sapiente, nihil libero. Pariatur doloremque,
+        recusandae commodi totam reprehenderit libero quas fuga odit voluptas
+        vero aliquid ducimus non distinctio ea repudiandae deleniti et earum
+        dolore? Expedita adipisci molestiae obcaecati officiis accusamus
+        dolorem, autem tempora iure maiores saepe cumque harum.
+      </div>
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut vitae
+        reiciendis perferendis a libero ipsum, porro possimus ullam saepe vel!
+        Quos, nesciunt architecto. Nam ipsum, laboriosam amet veritatis
+        dignissimos explicabo quasi vel. Odit et odio possimus, officia itaque
+        est ipsa obcaecati laborum. Modi expedita optio, maxime animi eum
+        beatae, ratione in ad aut debitis autem voluptatum officiis voluptatibus
+        amet corporis consectetur sapiente, nihil libero. Pariatur doloremque,
+        recusandae commodi totam reprehenderit libero quas fuga odit voluptas
+        vero aliquid ducimus non distinctio ea repudiandae deleniti et earum
+        dolore? Expedita adipisci molestiae obcaecati officiis accusamus
+        dolorem, autem tempora iure maiores saepe cumque harum.
+      </div>
     </div>
   );
 }
