e3c39f6bc164df0b06435cd3bee835c0a375ac03
huiyeon <yeon7513@nate.com>
2024-09-26 18:41:55 +0900

    20240926

--- src/api/simulationData.js
+++ src/api/simulationData.js
@@ -10,5 +10,18 @@ export const bestFarmInfo = async (type, query) => {
     return response.data.response.body.items.item;
   } catch (error) {
     console.error(error);
+
+    if (error.response) {
+      // 서버가 응답을 반환했을 경우
+      console.error('Response data:', error.response.data); // 응답 데이터
+      console.error('Response status:', error.response.status); // 상태 코드
+      console.error('Response headers:', error.response.headers); // 헤더
+    } else if (error.request) {
+      // 요청이 이루어졌지만 응답을 받지 못했을 경우
+      console.error('Request data:', error.request);
+    } else {
+      // 오류를 발생시킨 요청 설정
+      console.error('Error config:', error.config);
+    }
   }
 };
