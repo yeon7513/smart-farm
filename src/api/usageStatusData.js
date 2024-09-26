ca8ec426c68c4303f5e33b1ce5b8a04ec06422e2
huiyeon <yeon7513@nate.com>
2024-09-26 16:06:51 +0900

    20240926

--- src/api/usageStatusData.js
+++ src/api/usageStatusData.js
@@ -1,6 +1,6 @@
 // 전국 스마트팜 이용 현황
 const apiKey = process.env.REACT_APP_SMART_FARM_BIG_DATA_API_KEY;
-const apiUrl = `/smart/Agree_WS/webservices/ProvideRestService/getIdentityDataList/${apiKey}`;
+const apiUrl = `https://www.smartfarmkorea.net/Agree_WS/webservices/ProvideRestService/getIdentityDataList/${apiKey}`;
 
 export function usageStatusData() {
   let usageStatusResult = [];
