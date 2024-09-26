d54747928622f435565c8b0207bcc79c72032db8
seasukmin <seh9210@gmail.com>
2024-09-26 10:19:09 +0900

    240926

--- src/pages/my-farm/my-farm-list/dashboard/dashboard-contents/DashboardContent.js
+++ src/pages/my-farm/my-farm-list/dashboard/dashboard-contents/DashboardContent.js
@@ -1,14 +1,14 @@
-import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Outlet } from 'react-router-dom';
-import { useComponentContext } from '../../../../../context/ComponentContext';
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Outlet } from "react-router-dom";
+import { useComponentContext } from "../../../../../context/ComponentContext";
 import {
   countData,
   randomCountData,
-} from '../../../../../store/controlData/controlSlice';
-import DashboardSector from '../dashboard-nav/dashboard-sector/DashboardSector';
-import { fetchSectorInfo } from './../../../../../store/dashboard/dashboardSlice';
-import styles from './DashboardContent.module.scss';
+} from "../../../../../store/controlData/controlSlice";
+import DashboardSector from "../dashboard-nav/dashboard-sector/DashboardSector";
+import { fetchSectorInfo } from "./../../../../../store/dashboard/dashboardSlice";
+import styles from "./DashboardContent.module.scss";
 
 function DashboardContent({ docId }) {
   const { sectorInfo, isLoading } = useSelector(
@@ -29,7 +29,7 @@ function DashboardContent({ docId }) {
         if (prevCount >= 100) {
           return 0;
         }
-        return prevCount + 0.1;
+        return prevCount + 1;
       });
     }, 1000);
     return () => clearInterval(interval);
@@ -69,7 +69,7 @@ function DashboardContent({ docId }) {
   return (
     <div className={styles.content}>
       <ul className={styles.sectorMenu}>
-        {currComp !== 'Alert' &&
+        {currComp !== "Alert" &&
           [...sectorInfo]
             .sort((a, b) => a.id - b.id)
             .map((sector) => (
