5b8b5d85a6268b085d3f950df42c7c160da3a539
seasukmin <seh9210@gmail.com>
2024-09-26 14:46:54 +0900

    240926

--- src/pages/manager/chat-room-care/ChatRoomCare.js
+++ src/pages/manager/chat-room-care/ChatRoomCare.js
@@ -1,19 +1,15 @@
-import React, { useEffect } from 'react';
-import { TbMessageSearch } from 'react-icons/tb';
-import SearchBox from '../../../components/search_box/SearchBox';
-import styles from './ChatRoomCare.module.scss';
-
+import React, { useEffect } from "react";
+import { TbMessageSearch } from "react-icons/tb";
+import SearchBox from "../../../components/search_box/SearchBox";
+import styles from "./ChatRoomCare.module.scss";
+import { getOrder } from "../../../api/firebase";
 
 function ChatRoomCare() {
-
   return (
     <div className={styles.chatRoom}>
-     <SearchBox name={<TbMessageSearch />} placeholder={'채팅방 검색'} />
-ChatroomCare
-</div>
- 
+      <SearchBox name={<TbMessageSearch />} placeholder={"채팅방 검색"} />
+    </div>
   );
 }
-
 
 export default ChatRoomCare;
