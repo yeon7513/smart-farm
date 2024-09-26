0d37d713eb753c8959b0c5d9189e21ab56960cac
JuWon <yjw1732@gmail.com>
2024-09-26 18:55:27 +0900

    ..

--- src/components/chat-room/chat-room-header/ChatRoomHeader.js
+++ src/components/chat-room/chat-room-header/ChatRoomHeader.js
@@ -3,6 +3,7 @@ import backIcon from '../../../assets/main/backImg.svg';
 import closeIcon from '../../../assets/main/closeImg.svg';
 import styles from './ChatRoomHeader.module.scss';
 
+<<<<<<< Updated upstream
 function ChatRoomHeader({
   isLiveChatOpend,
   handleBackButtonClick,
@@ -66,6 +67,63 @@ function ChatRoomHeader({
       )}
     </div>
   );
+=======
+
+function ChatRoomHeader({ isLiveChatOpend,handleBackButtonClick,handleClose, isTransitioningToLiveChat, endChat }) {
+  return (
+    <div
+    className={`${styles.header} 
+    ${isLiveChatOpend ? styles.headerDetailOption : ""}  
+    ${isTransitioningToLiveChat ? styles.headerLiveChatting : "" }`}
+  >
+    {isTransitioningToLiveChat ? (
+  <>
+         <button className={styles.backBtn} onClick={() => { handleClose(); endChat(); }}>
+          <img
+            src={backIcon}
+            alt="뒤로 가기"
+            style={{ width: "16px", height: "16px" }}
+          />
+        </button>
+  <h2 className={styles.chattingTitle}>채팅 상담</h2>
+  <button className={styles.closeBtn} onClick={handleClose}>
+    <img src={closeIcon} alt="닫기" style={{ width: "16px", height: "16px" }} />
+  </button>
+</>
+) : isLiveChatOpend ? (
+      <>
+        <button className={styles.backBtn} onClick={handleBackButtonClick}>
+          <img
+            src={backIcon}
+            alt="뒤로 가기"
+            style={{ width: "16px", height: "16px" }}
+          />
+        </button>
+        <h2 className={styles.chatDetailTitle}>세부 선택</h2>
+        <button className={styles.closeBtn} onClick={handleClose}>
+          <img
+            src={closeIcon}
+            alt="닫기"
+            style={{ width: "16px", height: "16px" }}
+          />
+        </button>
+      </>
+    ) : (
+      <>
+        <h2 className={styles.chattingTitle}>아이팜 채팅상담</h2>
+        <button className={styles.closeBtn} onClick={() => { handleClose(); endChat(); }}>
+          <img
+            src={closeIcon}
+            alt="닫기"
+            style={{ width: "16px", height: "16px" }}
+          />
+        </button>
+        <p className={styles.guideText}>무엇을 도와드릴까요?</p>
+      </>
+    )}
+  </div>
+  )
+>>>>>>> Stashed changes
 }
 
 export default ChatRoomHeader;
