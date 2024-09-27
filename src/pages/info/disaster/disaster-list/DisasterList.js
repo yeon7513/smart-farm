6174c343035c36507fe82229d411620ecfc32aa9
je <wjddmsy12345@gmail.com>
2024-09-27 10:36:45 +0900

    .

--- src/pages/info/disaster/disaster-list/DisasterList.js
+++ src/pages/info/disaster/disaster-list/DisasterList.js
@@ -130,11 +130,7 @@ function DisasterList({ currentPage, itemsPerPage, updateTotalPages }) {
                         {/* 번호 매기기 */}
                       </div>
                       <div className={styles.title}>
-                        <p>
-                          {item.title.length > 15
-                            ? `${item.title.substring(0, 15)}...`
-                            : item.title}
-                        </p>
+                        <p>{item.title}</p>
                       </div>
                       <div className={styles.name}>관리자</div>
                       <div className={styles.date}>
