cff4735d82c04742c67f5dd83e907dbf3af09020
seasukmin <seh9210@gmail.com>
2024-09-30 10:44:15 +0900

    240930

--- src/context/MyPageMenu.js
+++ src/context/MyPageMenu.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import InfoEdit from '../pages/MyPage/info-edit/InfoEdit';
-import IntroMyPage from '../pages/MyPage/intro-my-page/IntroMyPage';
-import Payment from '../pages/MyPage/payment/Payment';
-import { useComponentContext } from './ComponentContext';
-import MYChatRoom from '../pages/MyPage/my-chat-room/MYChatRoom';
+import React from "react";
+import InfoEdit from "../pages/MyPage/info-edit/InfoEdit";
+import IntroMyPage from "../pages/MyPage/intro-my-page/IntroMyPage";
+import Payment from "../pages/MyPage/payment/Payment";
+import { useComponentContext } from "./ComponentContext";
+import MYChatRoom from "../pages/MyPage/my-chat-room/MYChatRoom";
 
 function MyPageMenu() {
   const { currComp } = useComponentContext();
 
   switch (currComp) {
-    case 'IntroMyPage':
+    case "IntroMyPage":
       return <IntroMyPage />;
-    case 'Payment':
+    case "Payment":
       return <Payment />;
-    case 'InfoEdit':
+    case "InfoEdit":
       return <InfoEdit />;
-    case 'MyChatRoom':
+    case "MyChatRoom":
       return <MYChatRoom />;
     // case 'Asinfo':
     //   return <Asinfo />;
--- src/lib/menu.js
+++ src/lib/menu.js
@@ -1,94 +1,94 @@
 export const paths = {
   gnb: [
-    { path: '/', name: '홈' },
+    { path: "/", name: "홈" },
     {
-      path: 'about',
-      name: '소개',
+      path: "about",
+      name: "소개",
       depth: [
         {
-          path: 'about',
-          name: '회사소개',
+          path: "about",
+          name: "회사소개",
         },
-        { path: '/about/service', name: '서비스소개' },
+        { path: "/about/service", name: "서비스소개" },
       ],
     },
     {
-      path: 'info',
-      name: '정보',
+      path: "info",
+      name: "정보",
       depth: [
-        { path: '/info', name: '이용 현황', comp: 'UsageStatus' },
+        { path: "/info", name: "이용 현황", comp: "UsageStatus" },
         {
-          path: '/info',
-          name: '시뮬레이션',
-          comp: 'Simulation',
+          path: "/info",
+          name: "시뮬레이션",
+          comp: "Simulation",
         },
-        { path: '/info', name: '병해충', comp: 'Diseases' },
+        { path: "/info", name: "병해충", comp: "Diseases" },
         {
-          path: '/info',
-          name: '자연재해',
-          comp: 'Disaster',
+          path: "/info",
+          name: "자연재해",
+          comp: "Disaster",
         },
       ],
     },
     {
-      path: 'community',
-      name: '커뮤니티',
+      path: "community",
+      name: "커뮤니티",
       depth: [
-        { path: '/community', name: '공지사항', comp: 'Notice' },
-        { path: '/community', name: 'FAQ', comp: 'Faq' },
-        { path: '/community', name: '정보 공유', comp: 'SharingInformation' },
-        { path: '/community', name: 'A/S 문의', comp: 'AfterService' },
+        { path: "/community", name: "공지사항", comp: "Notice" },
+        { path: "/community", name: "FAQ", comp: "Faq" },
+        { path: "/community", name: "정보 공유", comp: "SharingInformation" },
+        { path: "/community", name: "A/S 문의", comp: "AfterService" },
       ],
     },
     {
-      path: 'request',
-      name: '견적의뢰',
+      path: "request",
+      name: "견적의뢰",
     },
   ],
   spot: [
-    { path: '/login', name: '로그인' },
-    { path: '/register', name: '회원가입' },
+    { path: "/login", name: "로그인" },
+    { path: "/register", name: "회원가입" },
     // { path: '/mypage', name: '마이페이지' },
   ],
 };
 
 export const myPageSideMenu = [
   {
-    label: '내 정보',
+    label: "내 정보",
     menu: [
-      { comp: 'IntroMyPage', name: '내 정보 조회' },
-      { comp: 'InfoEdit', name: '내 정보 수정' },
+      { comp: "IntroMyPage", name: "내 정보 조회" },
+      { comp: "InfoEdit", name: "내 정보 수정" },
     ],
   },
   {
-    label: '결제',
-    menu: [{ comp: 'Payment', name: '결제 내역' }],
+    label: "결제",
+    menu: [{ comp: "Payment", name: "결제 내역" }],
   },
   {
-    label: '채팅상담',
-    menu: [{ comp: 'MyChatRoom', name: '채팅상담 문의 내역' }],
+    label: "채팅상담",
+    menu: [{ comp: "MyChatRoom", name: "채팅상담 문의 내역" }],
   },
 ];
 
 export const infoSideMenu = [
-  { comp: 'UsageStatus', name: '이용현황' },
-  { comp: 'Simulation', name: '시뮬레이션' },
-  { comp: 'Diseases', name: '병해충 정보' },
-  { comp: 'Disaster', name: '자연재해 정보' },
+  { comp: "UsageStatus", name: "이용현황" },
+  { comp: "Simulation", name: "시뮬레이션" },
+  { comp: "Diseases", name: "병해충 정보" },
+  { comp: "Disaster", name: "자연재해 정보" },
 ];
 
 export const communitySideMenu = [
-  { comp: 'Notice', name: '공지사항' },
-  { comp: 'Faq', name: 'FAQ' },
-  { comp: 'SharingInformation', name: '정보 공유' },
-  { comp: 'AfterService', name: 'A/S 문의' },
+  { comp: "Notice", name: "공지사항" },
+  { comp: "Faq", name: "FAQ" },
+  { comp: "SharingInformation", name: "정보 공유" },
+  { comp: "AfterService", name: "A/S 문의" },
 ];
 
 export const managerSideMenu = [
-  { comp: 'OverallStatus', name: '전체 현황' },
-  { comp: 'MembersCare', name: '회원 관리' },
-  { comp: 'QuotationsCare', name: '견적 관리' },
-  { comp: 'ComplaintsCare', name: '신고 관리' },
-  { comp: 'AfterServiceCare', name: 'A/S 관리' },
-  { comp: 'ChatRoomCare', name: '채팅상담 관리' },
+  { comp: "OverallStatus", name: "전체 현황" },
+  { comp: "MembersCare", name: "회원 관리" },
+  { comp: "QuotationsCare", name: "견적 관리" },
+  { comp: "ComplaintsCare", name: "신고 관리" },
+  { comp: "AfterServiceCare", name: "A/S 관리" },
+  { comp: "ChatRoomCare", name: "채팅상담 관리" },
 ];
