59ba414edfdce9d45ae81a6238f0e5c44c67ad6b
huiyeon <yeon7513@nate.com>
2024-09-30 13:47:13 +0900

    20240930

--- src/pages/info/simulation/simulation-select-data/SimulationSelectData.js
+++ src/pages/info/simulation/simulation-select-data/SimulationSelectData.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SimulationBtn from './simulation-btns/SimulationBtn';
 import styles from './SimulationSelectData.module.scss';
 
 function SimulationSelectData({ selectDatas, onClick }) {
-  console.log('selectDatas: ', selectDatas);
+  const [selectedIndexes, setSelectedIndexes] = useState({});
+
+  const handleClick = (dataName, count, index) => {
+    setSelectedIndexes((prev) => ({ ...prev, [dataName]: index }));
+    onClick(dataName, count);
+  };
 
   return (
     <div className={styles.selectBox}>
@@ -11,16 +16,21 @@ function SimulationSelectData({ selectDatas, onClick }) {
         <div key={idx} className={styles.content}>
           <h4 className={styles.label}>{data.name}</h4>
           <div className={styles.btns}>
-            {data.values.map((value, idx) => (
-              <SimulationBtn
-                key={idx}
-                name={data.name}
-                count={value.count}
-                onClick={onClick}
-              >
-                {value.range}
-              </SimulationBtn>
-            ))}
+            {data.values.map((value, valIdx) => {
+              const isSelected = selectedIndexes[data.name] === valIdx;
+
+              return (
+                <SimulationBtn
+                  key={valIdx}
+                  name={data.name}
+                  count={value.count}
+                  onClick={() => handleClick(data.name, value.count, valIdx)}
+                  className={isSelected ? styles.selected : ''}
+                >
+                  {value.range}
+                </SimulationBtn>
+              );
+            })}
           </div>
         </div>
       ))}
