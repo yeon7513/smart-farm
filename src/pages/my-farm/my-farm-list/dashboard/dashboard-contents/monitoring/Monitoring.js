a29f949b039fe66fc93334f4b713e507f9cd72ee
seasukmin <seh9210@gmail.com>
2024-09-30 17:30:10 +0900

    240930

--- src/pages/my-farm/my-farm-list/dashboard/dashboard-contents/monitoring/Monitoring.js
+++ src/pages/my-farm/my-farm-list/dashboard/dashboard-contents/monitoring/Monitoring.js
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchGrowthData } from "../../../../../../store/bestfarm/bestfarmSlice";
 
 function Monitoring() {
+  const dispatch = useDispatch();
+  const [farmState, setFarmState] = useState("");
+  const { growthData } = useSelector((state) => state.bestfarmSlice);
+
+  useEffect(() => {
+    dispatch(fetchGrowthData(`pageSize=10&searchFrmhsCode=${farmState}`));
+  }, [dispatch, farmState]);
+
+  useEffect(() => {
+    console.log(growthData);
+  }, []);
   return (
     <div>
       <span>Monitoring</span>
       <Outlet />
+      {/* <RenderingChart /> */}
     </div>
   );
 }
-
+// ** 차트 사용법
+//  - 데이터 모양
+//    -> const data = [ {name: 'string', value: number}, {name: 'string', value: number}, ... ];
 export default Monitoring;
