96070571740a20718063b56ebcce9812c0b2276e
LeeJh <4447447@naver.com>
2024-10-04 00:42:19 +0900

    Update RequestForm.js

--- src/pages/request/request-form/RequestForm.js
+++ src/pages/request/request-form/RequestForm.js
@@ -1,4 +1,4 @@
-import { addDoc, collection, doc, setDoc } from "firebase/firestore";
+import { addDoc, collection } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { db } from "../../../api/firebase";
@@ -9,7 +9,6 @@ import { convertingAddressToGeoCode } from "./../../../api/geoCode";
 import styles from "./RequestForm.module.scss";
 import {
   renameOptionsEn,
-  renameOptionsKor,
 } from "../../../utils/renameOptions";
 
 function RequestForm({ user }) {
